a06a942f1737651f0880ed62341462d95383445a
pinkjagnie <olga.krzysztofik@gmail.com>
2022-09-07 14:21:01 +0200

    Added addMovie API route

---
+++ pages/api/add.js
@@ -0,0 +1,35 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { title, year, image, time, director, writer, production } = req.body;
+
+    if (
+      !title ||
+      title.trim() === "" ||
+      !year ||
+      year.trim() === "" ||
+      !image ||
+      image.trim() === "" ||
+      !time ||
+      time.trim() === "" ||
+      !director ||
+      director.trim() === "" ||
+      !writer ||
+      writer.trim() === "" ||
+      !production ||
+      production.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input" });
+      return;
+    }
+  }
+
+  const newMovie = {title, year, image, time, director, writer, production, isFeatured: false};
+
+  console.log(newMovie);
+
+  res
+    .status(200)
+    .json({ message: "Successfully stored movie", message: newMovie });
+}
+
+export default handler;
