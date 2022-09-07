3a2a72d8a9f75811bd73ea72e669f158e6f3bfeb
pinkjagnie <olga.krzysztofik@gmail.com>
2022-09-07 13:40:05 +0200

    Added useRef hook to AddForm

--- components/add/add-form.js
+++ components/add/add-form.js
@@ -1,34 +1,56 @@
+import { useRef } from "react";
+
 import styles from "./add-form.module.css";
 
 function AddForm() {
+  const titleRef = useRef();
+  const yearRef = useRef();
+  const timeRef = useRef();
+  const directorRef = useRef();
+  const writerRef = useRef();
+  const productionRef = useRef();
+
+  const addMovieHandler = (event) => {
+    event.preventDefault();
+
+    const enteredTitle = titleRef.current.value;
+    const enteredYear = yearRef.current.value;
+    const enteredTime = timeRef.current.value;
+    const enteredDirector = directorRef.current.value;
+    const enteredWriter = writerRef.current.value;
+    const enteredProduction = productionRef.current.value;
+
+    console.log(enteredTitle, enteredYear, enteredTime, enteredDirector, enteredWriter, enteredProduction)
+  }
+
   return (
     <section className={styles.add}>
       <h1>Add a movie to our database</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={addMovieHandler}>
         <div className={styles.controls}>
           <div className={styles.control}>
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" required />
+            <input type="text" id="title" required ref={titleRef}/>
           </div>
           <div className={styles.control}>
             <label htmlFor="year">Year of production</label>
-            <input type="text" id="year" required />
+            <input type="text" id="year" required ref={yearRef}/>
           </div>
           <div className={styles.control}>
             <label htmlFor="time">Time (e.g. 2h 10min)</label>
-            <input type="text" id="time" required />
+            <input type="text" id="time" required ref={timeRef}/>
           </div>
           <div className={styles.control}>
             <label htmlFor="director">Director</label>
-            <input type="text" id="director" required />
+            <input type="text" id="director" required ref={directorRef}/>
           </div>
           <div className={styles.control}>
             <label htmlFor="writer">Writer</label>
-            <input type="text" id="writer" required />
+            <input type="text" id="writer" required ref={writerRef}/>
           </div>
           <div className={styles.control}>
             <label htmlFor="production">Production (country name)</label>
-            <input type="text" id="production" required />
+            <input type="text" id="production" required ref={productionRef}/>
           </div>
         </div>
         <div className={styles.actions}>
