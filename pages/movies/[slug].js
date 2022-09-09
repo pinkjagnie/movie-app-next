a98ad0ed7cb54a98886d672d5789518bbf73c693
pinkjagnie <olga.krzysztofik@gmail.com>
2022-09-09 23:24:55 +0200

    Fetching single movie by slug

--- pages/movies/[slug].js
+++ pages/movies/[slug].js
@@ -1,7 +1,28 @@
 import MovieContent from "../../components/movies/movie-detail/movie-content";
 
-function MovieDetailPage() {
-  return <MovieContent />;
+function MovieDetailPage(props) {
+  return <MovieContent movie={props.movie}/>;
 }
 
 export default MovieDetailPage;
+
+export async function getServerSideProps(context) {
+
+  const { params } = context;
+  const { slug } = params;
+
+  let res = await fetch("http://localhost:3000/api/movies", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  
+  let movies = await res.json();
+  let movieData = await movies.movies.find(movie => movie.slug === slug)
+
+   return {
+    props: {
+      movie: movieData,
+    }
+  };
+}
