c8de74ec431d13f883084bed7cf92ad17958c068
pinkjagnie <olga.krzysztofik@gmail.com>
2022-09-09 21:56:05 +0200

    Fetching movies on AllMovies component

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,24 +1,3 @@
-// async function handler(req, res) {
-//   if (req.method === 'GET') {
-//     async function fetchMovies() {
-//       const response = await fetch('https://test.axiomos.pl/api/reports', {
-//         mode: 'cors',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       })
-//       .then((res) => {
-//         return res.json()
-//       })
-//       .then((myJson) => {setData(myJson)})
-//       };
-    
-//   }
-// }
-
-// export default handler;
-
-
 import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
@@ -26,9 +5,7 @@ export default async function handler(req, res) {
   const db = client.db("movies");
   
   if (req.method === "GET") {
-
-      const allMovies = await db.collection("movies").find({}).toArray();
-      res.json({ status: 200, movies: allMovies });
-  
+    const allMovies = await db.collection("movies").find({}).toArray();
+    res.json({ status: 200, movies: allMovies });
   }
 }
